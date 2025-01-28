175a090335476ec7d9caee5279735858347544d9
maksenssss <maxence.chartier@viacesi.fr>
2025-01-28 09:22:22 +0100

    v0_tests
    je découvre l'environnement tout se passe bien :)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenue dans un programme interactif en C#!");
+
+        Console.Write("Entrez votre nom : ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Bonjour, {name} !");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Ceci est le message numéro {i}");
+        }
+    }
+}
